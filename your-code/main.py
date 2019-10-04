7af1722634f9b970c9c8504ae47e4f9fd90a28c0
loukjsmalbil <l.smalbil@students.uu.nl>
2019-10-04 11:03:43 +0200

    New line of code added

--- your-code/main.py
+++ your-code/main.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+print("Hello, world!")
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
